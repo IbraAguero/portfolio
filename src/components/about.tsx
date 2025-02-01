7e2af7ccf96b2bd65e32d3fe550722e96ff013c1
IbraAguero <ibrahimaguero@gmail.com>
2025-01-31 21:51:10 -0300

    feat: add about section

---
+++ src/components/about.tsx
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <section className="mt-36 mb-20 p-10 bg-zinc-800 rounded-2xl shadow-xl">
+      <p className="text-zinc-300 ">
+        Tengo 22 años y soy Técnico en Programación. Me especializo en
+        desarrollo web, utilizando tecnologías modernas como React, Next.js, y
+        más. Estoy comprometido con crear aplicaciones funcionales y eficientes,
+        siempre buscando mejorar y aprender nuevas herramientas. 🚀
+      </p>
+    </section>
+  );
+};
+export default About;
