44dbab9991a0f31638cd3e76752709501ce0c798
IbraAguero <ibrahimaguero@gmail.com>
2025-09-03 12:48:34 -0300

    feat: add age automatically

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,8 +1,13 @@
 const About = () => {
+  const birthDate = new Date(2002, 4, 28);
+  const today = new Date();
+
+  const age = today.getFullYear() - birthDate.getFullYear();
+
   return (
     <section className="mt-36 mb-20 p-10 bg-zinc-800 rounded-2xl shadow-xl">
       <p className="text-zinc-300 ">
-        Tengo 22 años y soy Técnico en Programación. Me especializo en
+        Tengo {age} años y soy Técnico en Programación. Me especializo en
         desarrollo web, utilizando tecnologías modernas como React, Next.js, y
         más. Estoy comprometido con crear aplicaciones funcionales y eficientes,
         siempre buscando mejorar y aprender nuevas herramientas. 🚀
